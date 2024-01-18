5e6681d6a77b597285fa3a0299593ba3c0e792b6
04maryam <itsmaryamali54@gmail.com>
2024-01-18 12:37:40 +0300

    react & django connected

---
+++ jobboardfe/src/components/home/Home.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/hello-world/')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
